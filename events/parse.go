a1fa9828ed7aeceb4546ad303108291477bd1ad3
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-11-19 00:03:08 -0800

    move readers to own dir

---
+++ events/parse.go
@@ -0,0 +1,73 @@
+package events
+
+import (
+	ethABI "github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	log "github.com/sirupsen/logrus"
+	"math/big"
+)
+
+type LogBridgeData struct {
+	User   common.Address
+	Asset  common.Address
+	Amount *big.Int
+	Id     [32]byte
+}
+
+type LogBridgeCancelData struct {
+	User common.Address
+	Id   [32]byte
+}
+
+func LogBridge(_abi ethABI.ABI, method string, vLog types.Log) (LogBridgeData, error) {
+	log.WithFields(log.Fields{
+		"bridge_address": vLog.Address,
+		"block":   vLog.BlockNumber,
+		"tx_hash": vLog.TxHash,
+		"method": method,
+	}).Info("New bridge event")
+
+	var data LogBridgeData
+	err := _abi.UnpackIntoInterface(&data, method, vLog.Data)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"bridge_address": vLog.Address,
+			"block":   vLog.BlockNumber,
+			"tx_hash": vLog.TxHash,
+			"method": method,
+		}).Error("Could not unpack event")
+		return LogBridgeData{}, err
+	}
+	data.User = common.HexToAddress(vLog.Topics[1].Hex())
+	data.Asset = common.HexToAddress(vLog.Topics[2].Hex())
+	data.Amount = vLog.Topics[3].Big()
+
+	return data, nil
+}
+
+func LogBridgeCancel(_abi ethABI.ABI, method string, vLog types.Log) (LogBridgeCancelData, error) {
+	log.WithFields(log.Fields{
+		"bridge_address": vLog.Address,
+		"block":   vLog.BlockNumber,
+		"tx_hash": vLog.TxHash,
+		"method": method,
+	}).Info("New bridge event")
+
+	var data LogBridgeCancelData
+	err := _abi.UnpackIntoInterface(&data, method, vLog.Data)
+	if err != nil {
+		log.WithFields(log.Fields{
+			"bridge_address": vLog.Address,
+			"block":   vLog.BlockNumber,
+			"tx_hash": vLog.TxHash,
+			"method": method,
+		}).Error("Could not unpack event")
+		return LogBridgeCancelData{}, err
+	}
+	data.User = common.HexToAddress(vLog.Topics[1].Hex())
+	// Debug ID not marshaling
+	log.Println(vLog.Topics)
+
+	return data, nil
+}
--- handler/run.go
+++ handler/run.go
@@ -3,14 +3,12 @@ package handler
 import (
 	"fmt"
 	ethABI "github.com/ethereum/go-ethereum/accounts/abi"
-	"github.com/ethereum/go-ethereum/common"
-	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/crypto"
 	log "github.com/sirupsen/logrus"
-	"math/big"
 	"puffinbridgebackend/blockchain/contractInteraction"
 	"puffinbridgebackend/config"
 	"puffinbridgebackend/contractABI"
+	"puffinbridgebackend/events"
 	"puffinbridgebackend/global"
 	"puffinbridgebackend/state"
 	"strings"
@@ -18,18 +16,6 @@ import (
 
 func RunHandler() {
 	listenToEvents()
-}
-
-type LogBridge struct {
-	User   common.Address
-	Asset  common.Address
-	Amount *big.Int
-	Id     [32]byte
-}
-
-type LogBridgeCancel struct {
-	User common.Address
-	Id   [32]byte
 }
 
 func listenToEvents() {
@@ -66,68 +52,16 @@ func listenToEvents() {
 		case vLog := <-event:
 			switch vLog.Log.Topics[0].Hex() {
 			case logBridgeInSigHash.Hex():
-				_, err = logBridge(bridgeABI, "BridgeIn", vLog.Log)
+				_, err = events.LogBridge(bridgeABI, "BridgeIn", vLog.Log)
 			case logBridgeOutSigHash.Hex():
-				_, err = logBridge(bridgeABI, "BridgeOut", vLog.Log)
+				_, err = events.LogBridge(bridgeABI, "BridgeOut", vLog.Log)
 			case logBridgeOutWarmSigHash.Hex():
-				_, err = logBridge(bridgeABI, "BridgeOutWarm", vLog.Log)
+				_, err = events.LogBridge(bridgeABI, "BridgeOutWarm", vLog.Log)
 			case logBridgeOutCanceledSigHash.Hex():
-				_, err = logBridgeCancel(bridgeABI, "BridgeCancel", vLog.Log)
+				_, err = events.LogBridgeCancel(bridgeABI, "BridgeCancel", vLog.Log)
 			}
 			state.Write([]byte("block"), []byte(vLog.Network.Name), []byte(fmt.Sprintf("%v", vLog.Log.BlockNumber)))
 
 		}
 	}
-}
-
-func logBridge(_abi ethABI.ABI, method string, vLog types.Log) (LogBridge, error) {
-	log.WithFields(log.Fields{
-		"bridge_address": vLog.Address,
-		"block":   vLog.BlockNumber,
-		"tx_hash": vLog.TxHash,
-		"method": method,
-	}).Info("New bridge event")
-
-	var data LogBridge
-	err := _abi.UnpackIntoInterface(&data, method, vLog.Data)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"bridge_address": vLog.Address,
-			"block":   vLog.BlockNumber,
-			"tx_hash": vLog.TxHash,
-			"method": method,
-		}).Error("Could not unpack event")
-		return LogBridge{}, err
-	}
-	data.User = common.HexToAddress(vLog.Topics[1].Hex())
-	data.Asset = common.HexToAddress(vLog.Topics[2].Hex())
-	data.Amount = vLog.Topics[3].Big()
-
-	return data, nil
-}
-
-func logBridgeCancel(_abi ethABI.ABI, method string, vLog types.Log) (LogBridgeCancel, error) {
-	log.WithFields(log.Fields{
-		"bridge_address": vLog.Address,
-		"block":   vLog.BlockNumber,
-		"tx_hash": vLog.TxHash,
-		"method": method,
-	}).Info("New bridge event")
-
-	var data LogBridgeCancel
-	err := _abi.UnpackIntoInterface(&data, method, vLog.Data)
-	if err != nil {
-		log.WithFields(log.Fields{
-			"bridge_address": vLog.Address,
-			"block":   vLog.BlockNumber,
-			"tx_hash": vLog.TxHash,
-			"method": method,
-		}).Error("Could not unpack event")
-		return LogBridgeCancel{}, err
-	}
-	data.User = common.HexToAddress(vLog.Topics[1].Hex())
-	// Debug ID not marshaling
-	log.Println(vLog.Topics)
-
-	return data, nil
 }
