f8d9053e09cd1f5e72dc16ae63c6a07303fc91f8
Thomas Parker <91673095+soloth@users.noreply.github.com>
2022-12-06 21:45:08 -0800

    increase message size

--- api/rest.go
+++ api/rest.go
@@ -13,8 +13,8 @@ import (
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize:  1024,
-	WriteBufferSize: 1024,
+	ReadBufferSize:  1024 * 30,
+	WriteBufferSize: 1024 * 30,
 }
 
 func startRest() {
